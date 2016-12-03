53e6408d944d6f3eb5d538210a10554ca6ef7189
Administrator <1564666023@qq.com>
2016-12-03 16:25:11 +0800

    添加list的搜索功能

--- src/com/two/listDemo.java
+++ src/com/two/listDemo.java
@@ -1,9 +1,6 @@
 package com.two;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Administrator on 2016/12/2.
@@ -18,22 +15,40 @@ public class listDemo {
         //index必需小于等于前面田间的个数
         list.add(3,"C#");
 
-        //list元素遍历，使用迭代器
-        System.out.println("列表元素:");
-        //System.out.println(list);
-        Iterator it = list.iterator();
-        while (it.hasNext()){
-            System.out.println(it.next());
-        }
+//        //list元素遍历，使用迭代器
+//        System.out.println("列表元素:");
+//        //System.out.println(list);
+//        Iterator it = list.iterator();
+//        while (it.hasNext()){
+//            System.out.println(it.next());
+//        }
+
+
+//        System.out.println("------逆向遍历-----");
+//
+//        ListIterator listIterator = list.listIterator();
+//        while (listIterator.hasPrevious()){
+//            System.out.println(listIterator.previous());
+//        }
+
 
         //使用get()函数获取指定位置的元素
-        while (true){
-        System.out.println("请输入要查找的位置：");
-        Scanner s = new Scanner(System.in);
-        int num  = s.nextInt();
-        if (num<=list.size()) {
-            System.out.println("第" + num + "元素是：" + list.get(num));
+//        while (true){
+//        System.out.println("请输入要查找的位置：");
+//        Scanner s = new Scanner(System.in);
+//        int num  = s.nextInt();
+//        if (num<=list.size()) {
+//            System.out.println("第" + num + "元素是：" + list.get(num));
+//            }
+//        }
+
+
+        ListIterator ist  = list.listIterator();
+        while (ist.hasNext()){
+            if (ist.next().equals("PHP")){
+                ist.add("HTML");
             }
         }
+        System.out.println(list);
     }
 }

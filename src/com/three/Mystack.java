50de80f490aaee8d3b41a956fb85dad34f325450
Administrator <1564666023@qq.com>
2016-12-08 19:28:37 +0800

    ..

---
+++ src/com/three/Mystack.java
@@ -0,0 +1,10 @@
+package com.three;
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+public class Mystack {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/three/Student.java
+++ src/com/three/Student.java
@@ -35,4 +35,22 @@ public class Student {
                 ", age=" + age +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        return name != null ? name.equals(student.name) : student.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
 }

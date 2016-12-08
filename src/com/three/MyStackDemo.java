6d05cd50418f9bf7dbcaa775e75a7f1a1c0e04b1
Administrator <1564666023@qq.com>
2016-12-08 21:44:12 +0800

    linkedList的使用

---
+++ src/com/three/MyStackDemo.java
@@ -0,0 +1,25 @@
+package com.three;
+
+
+/**
+ * Created by Administrator on 2016/12/8.
+ *
+ * 自定义栈的使用
+ *
+ */
+public class MyStackDemo {
+    public static void main(String[] args) {
+        Mystack mystack = new Mystack();
+        mystack.add("java");
+        mystack.add("php");
+        mystack.add("html");
+
+//        System.out.println(mystack.get());
+//        System.out.println(mystack.get());
+//        System.out.println(mystack.get());
+
+
+
+
+    }
+}
--- src/com/three/Mystack.java
+++ src/com/three/Mystack.java
@@ -1,10 +1,31 @@
 package com.three;
 
+import java.util.LinkedList;
+
 /**
  * Created by Administrator on 2016/12/8.
+ * 自定义栈的集合
  */
 public class Mystack {
-    public static void main(String[] args) {
+    private LinkedList link;
 
+    public Mystack(){
+        link = new LinkedList();
+    }
+
+    public void add(Object object){
+        link.addFirst(object);
+    }
+
+    public Object  get() {
+        //return link.getFirst();
+        return link.removeFirst();
+    }
+
+    public int size(){
+        return link.size();
+    }
+    public boolean aa(){
+        return link.isEmpty();
     }
 }

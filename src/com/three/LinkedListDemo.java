52ec16f0dd79840d68ed9945a9fe54828fecf066
Administrator <1564666023@qq.com>
2016-12-08 19:27:43 +0800

    Linkedlist的使用

---
+++ src/com/three/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.three;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst("a");
+        list.addFirst("b");
+        list.addFirst("c");
+        list.addFirst("d");
+    }
+}
